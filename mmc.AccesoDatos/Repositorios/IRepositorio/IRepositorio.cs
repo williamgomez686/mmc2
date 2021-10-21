438c31704c347b0ba98960482067ada99dda91e9
William_Gomez_i7 <williamgomez686@hotmail.com>
2021-10-21 02:10:29 -0600

    Agregando Interfaz de repositorio

---
+++ mmc.AccesoDatos/Repositorios/IRepositorio/IRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mmc.AccesoDatos.Repositorios.IRepositorio
+{
+    public interface IRepositorio<T> where T :class
+    {
+        T Obtener(int id);
+        IEnumerable<T> ObtenerTodos(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string incluirPropiedades = null
+            );
+
+        IEnumerable<T> ObtenerPrimero(
+            Expression<Func<T, bool>> filter = null,
+            string incluirPropiedades = null
+            );
+
+        void Agregar(T entidad);
+
+        void Remover(int id);
+        void Remover(T entidad);
+        void removerRango(IEnumerable<T> entidad);
+    }
+}
