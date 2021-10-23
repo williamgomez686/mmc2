c81fea0de80995db386af31f1efffec92856c27a
William_Gomez_i7 <williamgomez686@hotmail.com>
2021-10-23 01:59:34 -0600

    Creando Controlador Estados y creando un metodo Obtener todos

---
+++ mmc/Areas/Admin/Controllers/EstadosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mmc.AccesoDatos.Repositorios.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mmc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EstadosController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public EstadosController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region Api
+        
+        [HttpGet]
+        public IActionResult ObtenerTodos()
+        {
+            var todos = _unidadTrabajo.Estado.ObtenerTodos();
+
+            return Json(new { data = todos});
+        }
+
+        #endregion
+    }
+}
