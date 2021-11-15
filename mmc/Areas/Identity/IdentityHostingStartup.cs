73b5359ca4d3ca283cd8a7a0d2f4c2bb6d3a9a11
William Gomez <wgomez@moticacioncristiana.org>
2021-11-15 17:03:40 -0600

    Agregando vistas de Identity con scaffolding

---
+++ mmc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using mmc.AccesoDatos.Data;
+
+[assembly: HostingStartup(typeof(mmc.Areas.Identity.IdentityHostingStartup))]
+namespace mmc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
