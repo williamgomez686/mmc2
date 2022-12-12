ffd2d6b3e560cfec27d5f8116011bbf874a235a5
William Gomez <wgomez@moticacioncristiana.org>
2022-12-12 10:32:48 -0600

    Aregalando boton

---
+++ mmc.AccesoDatos/Migrations/20221208210405_MigracionProduccion2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mmc.AccesoDatos.Migrations
+{
+    public partial class MigracionProduccion2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
