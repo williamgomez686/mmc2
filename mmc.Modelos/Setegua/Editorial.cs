3df6b9f04d3027747b289ac19ac2fb40ff39b376
williamgomez686 <williamgomez686@gmail.com>
2023-02-16 15:48:16 -0600

    AUTORES

---
+++ mmc.Modelos/Setegua/Editorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mmc.Modelos.Setegua
+{
+    public class Editorial:BaseModel
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Nombre de la Editorial")]
+        public string Nombre { get; set; }
+    }
+}
